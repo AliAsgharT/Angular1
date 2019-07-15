6614b32c07e1497b48aee9c860ddb08e3688d973
ALiAsghar <tavakkoli.asr@gmail.com>
2019-07-15 19:10:39 +0430

    return to mastergit status

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -5,47 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-
 export class ModalComponent implements OnInit {
 
-  constructor() {
-  }
-  private bodyText: string;
-
-  public modals: any[] = [];
+  constructor() { }
 
   ngOnInit() {
-    this.bodyText = 'This text can be updated in modal 1';
   }
 
-  add(modal: any) {
-    // add modal to array of active modals
-    this.modals.push(modal);
-  }
-
-  remove(id: string) {
-    // remove modal from array of active modals
-    this.modals = this.modals.filter(x => x.id !== id);
-  }
-
-  open(id: string) {
-    // open modal specified by id
-    const modal: any = this.modals.filter(x => x.id === id)[0];
-    modal.open();
-  }
-
-  close(id: string) {
-    // close modal specified by id
-    const modal: any = this.modals.filter(x => x.id === id)[0];
-    modal.close();
-  }
-
-  openModal(id: string) {
-    this.open(id);
-  }
-
-  closeModal(id: string) {
-    this.close(id);
-  }
 }
+
+
 
